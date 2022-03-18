df06c11c7a7e8d7f8e548b3df160195f9de21480
Nguyễn Hữu Khương Duy <2051063862@e.tlu.edu.vn>
2022-03-18 12:57:04 +0700

    them sap xep

--- js/yeu_cau_2.js
+++ js/yeu_cau_2.js
@@ -1,8 +1,19 @@
 const array =[];
-function create_array(){
+function tao_mang(){
     array.splice(0,array.length)
-    for(let i=0; i<Math.floor(Math.random() * 30)+1; i++) {
-        array.push(Math.floor(Math.random()*100)+1);
+    document.getElementById("array_arrange").innerHTML = array;
+
+    for(let i=0; i<Math.floor(Math.random() * 30)+5; i++) {
+        array.push(Math.floor(Math.random()*100));
     }
     document.getElementById("array1").innerHTML = array
+}
+
+function sap_tang(){
+    document.getElementById("array_arrange").innerHTML = array.sort();
+}
+
+function sap_giam(){
+    array.sort();
+    document.getElementById("array_arrange").innerHTML = array.reverse();
 }
